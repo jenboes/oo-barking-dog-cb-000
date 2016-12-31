81bef2e721618de42724cca92febc14db6118d7d
jenboes <jlboes@ameritech.net>
2016-12-31 16:23:26 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,27 @@
-# Your code goes here!
+class Dog
+
+  def name=(fullname)
+    @name = fullname
+  end
+
+  def name
+    @name
+  end
+
+  def breed=(fullname)
+    @breed = fullname
+  end
+
+  def breed
+    @breed
+  end
+
+  def bark
+    puts "woof!"
+  end
+
+  def initialize (dogname="Jeff", dogbreed="Mutt")
+    @name = dogname
+    @breed = dogbreed
+  end
+end
